2cce1a392264f08f73e0c21377a92d19e2ce3483
drammopo <drammopo@gmail.com>
2014-05-06 18:41:51 +0200

    Added logic

--- recipe-converter/recipe_converter.rb
+++ recipe-converter/recipe_converter.rb
@@ -1,3 +1,25 @@
 class RecipeConverter
+  attr_reader :ingredients
 
+  def initialize(ingredients)
+    @ingredients = ingredients
+  end
+
+  def to_tablespoon
+    result = []
+    ingredients.each do |ingredient|
+      ingredient.each do |name,grams|
+        result << ingredient[name] = grams / 15
+      end
+    end
+  end
+
+  def to_cups
+    result = []
+    ingredients.each do |ingredient|
+      ingredient.each do |name,grams|
+        result << ingredient[name] = ((grams / 15) / 16)
+      end
+    end
+  end
 end
--- recipe-converter/recipe_converter_test.rb
+++ recipe-converter/recipe_converter_test.rb
@@ -1,3 +1,4 @@
+gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative 'recipe_converter'
@@ -6,19 +7,19 @@ class RecipeConverterTest < MiniTest::Test
 
   def test_initialized_with_no_ingredients
     recipe_converter = RecipeConverter.new([])
-    assert_equal [] , recipe_converter.list
+    assert_equal [] , recipe_converter.ingredients
   end
 
   def test_initialized_with_one_ingredient
     ingredients = [{:salt => 5}]
     recipe_converter = RecipeConverter.new(ingredients)
-    assert_equal  [{:salt => 5}], recipe_converter.list
+    assert_equal  [{:salt => 5}], recipe_converter.ingredients
   end
 
   def test_initialized_with_two_ingredients
     ingredients = [{"salt" => 5}, {"olive oil" => 20}]
     recipe_converter = RecipeConverter.new(ingredients)
-    assert_equal [{"salt" => 5}, {"olive oil" => 20}], recipe_converter.list
+    assert_equal [{"salt" => 5}, {"olive oil" => 20}], recipe_converter.ingredients
   end
 
   def test_converting_to_tablespoon_with_no_ingredients
@@ -35,7 +36,7 @@ class RecipeConverterTest < MiniTest::Test
   def test_converting_to_tablespoon_with_two_ingredients
     ingredients = [{"salt" => 15}, {"olive oil" => 30}]
     recipe_converter = RecipeConverter.new(ingredients)
-    assert_equal [{"salt" => 1}, {"olive oil" => 2}], recipe_converter.to_tablespoon
+    assert_equal [{"salt" => 1}, {"olive oil" => 22}], recipe_converter.to_tablespoon
   end
 
   def test_converting_to_cups_with_no_ingredients
