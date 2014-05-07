2a43e0cd32d2b7a0a1c05dc47c2e041bcd69c202
drammopo <drammopo@gmail.com>
2014-05-07 15:06:28 +0200

    Added dog hash. All specs passing.

---
+++ hash-2.0/dog.rb
@@ -0,0 +1,21 @@
+DOG = {
+  :cattle_dog => {
+    preferences: [{name: "Harleigh"}],
+    names: ['Harleigh', 'brown'],
+ },
+  "harleigh"=> {
+    preferences: [{favorite_toy: "frog"}, {favorite_food: "chicken"}],
+    :information => {
+      sport:
+        ["frisbee", "hiking"]
+      }
+  },
+  "trixie"=> {
+    preferences: [{favorite_toy: "frog"}, {favorite_food: "steak"}],
+    :information => {
+      sport:
+        [:swimming, :prancing]
+      }
+    }
+  }
+
