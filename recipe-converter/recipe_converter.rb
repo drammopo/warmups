49bb0e87a9813b7636b1ea988f49d44fad343de0
drammopo <drammopo@gmail.com>
2014-05-07 09:33:44 +0200

    Adjusted indentation

--- recipe-converter/recipe_converter.rb
+++ recipe-converter/recipe_converter.rb
@@ -2,27 +2,40 @@ class RecipeConverter
   attr_reader :ingredients
 
   GRAMS_TO_A_TABLESPOON = 15
-  TABLESPOONS_TO_A_CUP = 16
+  TABLESPOONS_TO_A_CUP  = 16
 
   def initialize(ingredients)
     @ingredients = ingredients
   end
 
   def to_tablespoon
+    convert_to(:tablespoons)
+  end
+
+  def to_cups
+    convert_to(:cups)
+  end
+
+  private
+
+  def convert_to(type)
     result = []
     ingredients.each do |ingredient|
       ingredient.each do |name,grams|
-        result << ingredient[name] = grams / GRAMS_TO_A_TABLESPOON
+        if type == :tablespoons
+          result << ingredient[name] = conversion_to_cups(grams)
+        elsif type == :cups
+          result << ingredient[name] = conversion_to_tablespoons(grams)
+        end
       end
     end
   end
 
-  def to_cups
-    result = []
-    ingredients.each do |ingredient|
-      ingredient.each do |name,grams|
-        result << ingredient[name] = ((grams / GRAMS_TO_A_TABLESPOON.to_f) / TABLESPOONS_TO_A_CUP.to_f)
-      end
-    end
+  def conversion_to_cups(grams)
+    (grams / GRAMS_TO_A_TABLESPOON.to_f)
+  end
+
+  def conversion_to_tablespoons(grams)
+    ((grams / GRAMS_TO_A_TABLESPOON.to_f) / TABLESPOONS_TO_A_CUP.to_f)
   end
 end
