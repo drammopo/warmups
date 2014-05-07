f19f663dc6aceb5d6e2546933c51e981d0ac6407
drammopo <drammopo@gmail.com>
2014-05-07 23:05:48 +0200

    Finished exercise

--- employee-filter/filter.rb
+++ employee-filter/filter.rb
@@ -1,3 +1,10 @@
+require 'date'
 require_relative 'data'
 
 # write code that will return an array of employees who started before 2006
+hired_before_2006 = []
+EMPLOYEES.map do |employee|
+  year = employee[:start_date].strftime('%Y').to_i
+  hired_before_2006 << employee if year < 2006
+end
+print hired_before_2006
--- employee-filter/format.rb
+++ employee-filter/format.rb
@@ -1,3 +1,9 @@
+require 'date'
 require_relative 'data'
 
 # write code that will return an array of strings in the format "First Last (Title) - started m/d/yyyy"
+formatted_info = []
+EMPLOYEES.map do |employee|
+  formatted_info << "#{employee[:first_name]} #{employee[:last_name]} (#{employee[:title]}) - started #{employee[:start_date].strftime('%m/%d/%Y')}"
+end
+print formatted_info
