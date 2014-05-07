53a536242f7a3e18d647f8c3b4462c522b80c3e3
drammopo <drammopo@gmail.com>
2014-05-07 22:26:41 +0200

    Added exercise files as per gist

---
+++ employee-filter/filter.rb
@@ -0,0 +1,3 @@
+require_relative 'data'
+
+# write code that will return an array of employees who started before 2006
---
+++ employee-filter/format.rb
@@ -0,0 +1,3 @@
+require_relative 'data'
+
+# write code that will return an array of strings in the format "First Last (Title) - started m/d/yyyy"
