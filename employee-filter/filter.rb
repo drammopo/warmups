5dfe5d44f08c2d351af1068b022ba40447b2b10d
drammopo <drammopo@gmail.com>
2014-05-08 06:34:23 +0200

    Use Date#year instead of #strftime and the #to_i

--- employee-filter/filter.rb
+++ employee-filter/filter.rb
@@ -4,7 +4,6 @@ require_relative 'data'
 # write code that will return an array of employees who started before 2006
 hired_before_2006 = []
 EMPLOYEES.map do |employee|
-  year = employee[:start_date].strftime('%Y').to_i
-  hired_before_2006 << employee if year < 2006
+  hired_before_2006 << employee if employee[:start_date].year < 2006
 end
 print hired_before_2006
