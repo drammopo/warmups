15ff33d087ae56f6ef939d5cf8829ab28f266427
drammopo <drammopo@gmail.com>
2014-05-10 22:33:11 +0200

    Added random voice selection

--- alarm-clock/my_clock.rb
+++ alarm-clock/my_clock.rb
@@ -1,21 +1,30 @@
 length_of_time = ARGV[0].to_i
 unit = ARGV[1]
 
-converted_unit = case
-when unit.match(/hours/) then 3600
-when unit.match(/minute/) then 60
-when unit.match(/seconds/) then 1
-else 10
+HOURS = 3600
+MINUTES = 60
+SECONDS = 1
+
+voice_names = ["Agnes", "Albert", "Alex", "Bad News", "Bahh", "Bells", "Boing", "Bruce","Bubbles", "Cellos", "Deranged", "Fred", "Good News", "Hysterical", "Ioana", "Junior", "Kathy", "Pipe Organ", "Princess", "Ralph", "Trinoids", "Vicki", "Victoria", "Whisper", "Xander", "Yelda", "Zosia", "Zuzana"]
+
+def end_time(start_time, n, unit)
+  start_time + (n * unit)
 end
 
-start_time = Time.now
-end_time = start_time + (length_of_time * converted_unit)
+def convert_unit_of_measure(str)
+  converted_unit = case
+  when str.match(/hours/) then HOURS
+  when str.match(/minute/) then MINUTES
+  when str.match(/seconds/) then SECONDS
+  else 10
+  end
+end
 
-puts "This script will stop around #{end_time}"
+end_time = end_time(Time.now, length_of_time, convert_unit_of_measure(unit))
 
 while true
-  if Time.now == end_time
-    system("say \"Time\'s up\!\"")
+  if Time.now >= end_time
+    system("say -v \"#{voice_names.sample}\" \"Time\'s up\!\"")
     exit
   end
 end
